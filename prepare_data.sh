ac0097ebe90092ef3c1808c51bfe69f096ab27a9
Adel Rahimi <rahimi.adel@gmail.com>
2018-11-23 16:18:56 +0330

    Added main file for preparing data

---
+++ prepare_data.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo Downloading ELMO pre-trained models
+wget https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway_5.5B/elmo_2x4096_512_2048cnn_2xhighway_5.5B_weights.hdf5
