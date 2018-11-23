1fb5455c806ac4a62dd21f211bf44d3d5e8deaba
Adel Rahimi <rahimi.adel@gmail.com>
2018-11-23 16:21:25 +0330

    Update prepare_data.sh

--- prepare_data.sh
+++ prepare_data.sh
@@ -2,3 +2,4 @@
 
 echo Downloading ELMO pre-trained models
 wget https://s3-us-west-2.amazonaws.com/allennlp/models/elmo/2x4096_512_2048cnn_2xhighway_5.5B/elmo_2x4096_512_2048cnn_2xhighway_5.5B_weights.hdf5
+echo Finnished downloading the model
