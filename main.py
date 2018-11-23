f8ca1e62c749c91c680d71dbf9a635ba395c7d6a
Adel Rahimi <rahimi.adel@gmail.com>
2018-11-23 16:23:52 +0330

    first draft of the project

---
+++ main.py
@@ -0,0 +1,12 @@
+# use either ELMO or BERT
+# imports
+
+# load weights
+
+# get the vectors for the input strings
+
+# compare the distance, for now the cosine similarity
+
+#TODO: try different measures
+
+# output the chance of being similar
