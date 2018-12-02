c43690e7c7384a36c1e9ee3411e5edae5f7344ac
Adel Rahimi <rahimi.adel@gmail.com>
2018-12-02 16:07:13 +0330

    Create elmo-load.py
    + elmo

---
+++ elmo-load.py
@@ -0,0 +1,29 @@
+# https://allennlp.org/elmo
+
+import tensorflow as tf
+from keras import models
+from numpy import linalg as LA
+from allennlp.commands.elmo import ElmoEmbedder
+import scipy
+import numpy as np
+
+# load weights
+options_file = 'elmo_2x4096_512_2048cnn_2xhighway_5.5B_options.json'
+weight_file = 'elmo_2x4096_512_2048cnn_2xhighway_5.5B_weights.hdf5'
+
+
+elmo = ElmoEmbedder(options_file, weight_file)
+while True:
+    x = input('input sentence one: ').split()
+    y = input('input sentence two: ').split()
+    vectors1 = elmo.embed_sentence(x)
+    vectors2 = elmo.embed_sentence(y)
+
+    word_level_distancs = (abs(LA.norm(vectors1[0]) - LA.norm(vectors2[0])))
+    syntax_level_distancs = (abs(LA.norm(vectors1[1]) - LA.norm(vectors2[1])))
+    semantics_level_distancs = (abs(LA.norm(vectors1[2]) - LA.norm(vectors2[2])))
+
+    print('Word level distance', word_level_distancs)
+    print('Syntax level distance', syntax_level_distancs)
+    print('Semantics level distance', semantics_level_distancs)
+    # TODO: try different measures
